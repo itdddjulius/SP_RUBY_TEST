c08f804d9a11be5c69015d4bf60bfe3f55bf09fa
It Ddd <itddd.julius@gmail.com>
2019-07-16 17:00:06 +0100

    JO - TUE16JUL2019
    JO - TUE16JUL2019

--- parser.rb
+++ parser.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby 
+# ===================================================================================
+# MODULE : parser.rb
+#
+# AUTHOR : Julius Olatokunbo
+#
+# WRITTEN: TUE - 16 July 2019
+#
+# REASON : Ruby Developer Test
+# The test is as follows:
+#  1. ruby_app
+#  Write a ruby script that:
+#  a. Receives a log as argument (webserver.log is provided)
+#  e.g.: ./parser.rb webserver.log
+#  b. Returns the following:
+#  > list of webpages with most page views ordered from most pages views to less page views
+#  e.g.:
+#  /home 90 visits /index 80 visits etc... > list of webpages with most
+#  unique page views also ordered
+#  e.g.:
+#  /about/2 8 unique views
+#  /index 5 unique views etc...
+#
+# ===================================================================================
 
 class Parser
   def parse
